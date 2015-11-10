493625e4b653d8079d0e0025e34e920bea471234
Nathan Fisher <nfisher@junctionbox.ca>
2015-11-10 20:52:55 +0000

    Add consul.sh for local testing.

---
+++ consul.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -exu
+
+PLAT=`uname -s`
+ARCH=`uname -m`
+
+export GOMAXPROCS=2
+
+./consul/${PLAT}/${ARCH}/consul agent -server -bootstrap-expect 1 -data-dir consul/data -ui-dir consul/ui &
+CONSUL=$!
